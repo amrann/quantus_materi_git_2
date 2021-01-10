a54b77eba3577ee2f518c14726c451c47e6a130d
amrann <amr.bin.an@gmail.com>
2021-01-10 19:34:33 +0700

    edit again

--- main.py
+++ main.py
@@ -5,5 +5,8 @@ def test_luas_prisma_segitiga():
     luas_prismasegitiga = luas.PrismaSegitiga()
     hasil = luas_prismasegitiga.hitung(6, 7, 5, 3, 4)
     print ("Luas Prisma Segitiga = ", hasil)
+    
+def test_luas_prisma_segitiga():
+    print ("coba")
 
 test_luas_prisma_segitiga()
