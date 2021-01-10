b9ee3ab716999cbe1da81352dce99987bc8b3e74
amrann <amr.bin.an@gmail.com>
2021-01-10 19:27:54 +0700

    tambah file volume

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import luas
 
-
 def test_luas_prisma_segitiga():
     luas_prismasegitiga = luas.PrismaSegitiga()
     hasil = luas_prismasegitiga.hitung(6, 7, 5, 3, 4)
--- volume.py
+++ volume.py
@@ -1,1 +1,7 @@
-#kubus,balok,kerucut,bola,tabung,limas segitiga,prisma segitiga
+#kubus, balok, kerucut, bola, tabung, limas segitiga, prisma segitiga
+
+import luas
+
+class VolumePrismaSegitiga(luas.LuasSegititiga):
+    def hitung(self, alas:float, tinggi:float):
+        return alas*tinggi
