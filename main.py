8d93f5219c0c8fbaea3f8c3bbc77ec3e14f076c1
amrann <amr.bin.an@gmail.com>
2021-01-10 19:39:38 +0700

    add function volume prisma segitiga

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ def test_luas_prisma_segitiga():
     hasil = luas_prismasegitiga.hitung(6, 7, 5, 3, 4)
     print ("Luas Prisma Segitiga = ", hasil)
     
-def test_luas_prisma_segitiga():
-    print ("coba")
+def test_volume_prisma_segitiga():
+    volume_prismasegitiga = volume.VolumePrismaSegitiga()
+    hasil = volume_prismasegitiga.hitung(6, 7)
+    print("Volume Prisma Segitiga = ", hasil)
 
 test_luas_prisma_segitiga()
